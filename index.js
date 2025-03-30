c4fd538b7e9a8b41aba8b4c7faaf85c8a08c3050
Weiting Kuo <kuowesley1997@gmail.com>
2025-03-30 01:00:06 -0400

    update

--- index.js
+++ index.js
@@ -58,5 +58,11 @@ trackButton.addEventListener("click", async () => {
 document.getElementById("logoutBtn").addEventListener("click", () => {
     // For example, remove the token and redirect to the login page
     localStorage.removeItem("loggedInUser");
-    window.location.href = "/login.html";
+    window.location.href = "login.html";
+});
+
+document.getElementById("analysisBtn").addEventListener("click", () => {
+    // For example, remove the token and redirect to the login page
+    localStorage.removeItem("loggedInUser");
+    window.location.href = "analysis.html";
 });
